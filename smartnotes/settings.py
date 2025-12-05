a431c2a246c928a12a82337dd5369a63dd159dfc
Alex Hill <achill113@gmail.com>
2025-12-05 15:36:02 -0500

    Fix environment setup

--- smartnotes/settings.py
+++ smartnotes/settings.py
@@ -11,9 +11,12 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+load_dotenv(BASE_DIR / ".env")
 
 
 # Quick-start development settings - unsuitable for production
@@ -83,7 +86,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'smartnotes',
         'USER': 'postgres',
-        'PASSWORD': 'flag4-ewe-baulk',
+        'PASSWORD': os.getenv("POSTGRES_PASSWORD"),
         'HOST': '127.0.0.1',
         'PORT': '5433'
     }
